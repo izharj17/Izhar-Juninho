9f9a4b6aee96387af9f10c94f67483115826de17
RaditPasya <radityaphr@gmail.com>
2024-06-26 09:17:38 +0700

    update kalkulasi poin KPI

--- kpi_teacher/models/kpi_teacher.py
+++ kpi_teacher/models/kpi_teacher.py
@@ -1,4 +1,5 @@
 from odoo import models, fields, api
+import math
 
 class KpiTeacher(models.Model):
     _name = 'kpi.teacher'
@@ -10,27 +11,34 @@ class KpiTeacher(models.Model):
     department_id = fields.Many2one('hr.department', 'Bagian', related='guru_id.department_id')
     template_question_id = fields.Many2one('master.question', 'Jenis KPI ', required=True)
     kpi_ids = fields.One2many('kpi.teacher.line', 'kpi_teacher_id', string='KPI Line')
-    tanggal_kpi = fields.Date('Tanggal', required=True, default=lambda self: fields.Date.today(),
-        tracking=True)
-    nilai_total_kpi = fields.Integer('Nilai Total')
+    tanggal_kpi = fields.Date('Tanggal', required=True, default=lambda self: fields.Date.today(), tracking=True)
+    nilai_total_kpi = fields.Float('Nilai Total', compute='_compute_nilai_total_kpi', store=True, readonly=True)
     nama_sekolah = fields.Many2one('res.company', 'Sekolah')
 
     @api.onchange('template_question_id')
     def _onchange_template_question_id(self):
         if self.template_question_id:
-            # Hapus baris KPI yang ada
             self.kpi_ids.unlink()
-
-            # Ambil pertanyaan dari MasterQuestion dan buat baris KPI
             for question_line in self.template_question_id.master_question_ids:
                 kpi_line_vals = {
                     'kpi_teacher_id': self.id,
                     'question': question_line.question,
-                    'ketuntasan': '',  # Anda dapat menetapkan nilai default untuk 'jawaban' jika diperlukan
+                    'ketuntasan': False,
                     'file_kpi': '',
                 }
                 self.kpi_ids.create(kpi_line_vals)
 
+    @api.depends('kpi_ids.ketuntasan')
+    def _compute_nilai_total_kpi(self):
+        for record in self:
+            number_of_questions = len(record.kpi_ids)
+            if number_of_questions > 0:
+                point_per_question = 100 / number_of_questions
+                completed_questions = sum(1 for line in record.kpi_ids if line.ketuntasan)
+                total_score = point_per_question * completed_questions
+                record.nilai_total_kpi = min(total_score, 100)  # Cap the score at 100
+            else:
+                record.nilai_total_kpi = 0
 
 class KpiTeacherLine(models.Model):
     _name = 'kpi.teacher.line'
@@ -38,6 +46,11 @@ class KpiTeacherLine(models.Model):
     _description = "KPI Line"
 
     kpi_teacher_id = fields.Many2one('kpi.teacher', string='Survey')
-    question = fields.Text('Nama Kegiatan', size=2)
-    ketuntasan = fields.Boolean('Ketuntasan', default=True, tracking=True, size=4)
-    file_kpi = fields.Binary('Unggah Dokumen', file_kpi=True, size=4)
+    question = fields.Text('Nama Kegiatan')
+    ketuntasan = fields.Boolean('Ketuntasan', default=False, tracking=True)
+    file_kpi = fields.Binary('Unggah Dokumen')
+
+    @api.onchange('ketuntasan')
+    def _onchange_ketuntasan(self):
+        if self.kpi_teacher_id:
+            self.kpi_teacher_id._compute_nilai_total_kpi()
