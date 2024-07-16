0a83ddc86298146c1535db52aafc4ee0eea0b199
izharj17 <123434828+izharj17@users.noreply.github.com>
2024-07-16 11:46:39 +0700

    Update hr_inherit_attendance.py

--- hr_inherit_attendance/models/hr_inherit_attendance.py
+++ hr_inherit_attendance/models/hr_inherit_attendance.py
@@ -4,10 +4,8 @@ class hr_inherit_attendance(models.Model):
     _inherit = "hr.attendance"
 
     status_kehadiran = fields.Selection([
-        ('ot', 'Ontime'),
+        ('ot', 'Tepat Waktu'),
         ('tlb', 'Terlambat'),
-        ('skt', 'Sakit'),
-        ('i', 'Ijin')
     ], string='Status')
     long_position = fields.Char('Long Position')
     lat_position = fields.Char('Lat Position')
