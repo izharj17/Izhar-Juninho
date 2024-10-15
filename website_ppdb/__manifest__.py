33738fd1986df10a2ab9edc5b11a5e574a940e0b
izharj17 <123434828+izharj17@users.noreply.github.com>
2024-10-15 15:39:19 +0700

    Website PPDB Default
    Kerangka Dasar Addon Form PPDB Online based on odoo14

---
+++ website_ppdb/__manifest__.py
@@ -0,0 +1,16 @@
+{
+    'name': 'Website PPDB',
+    'summary': 'PPDB Formulir Pendaftaran Front-End',
+    'description': 'Module untuk menampilkan formulir PPDB pada website',
+    'author': 'Your Name',
+    'website': 'https://yourwebsite.com',
+    'category': 'Website',
+    'version': '1.0',
+    'depends': ['website', 'base', 'mail'],
+    'data': [
+        'views/website_ppdb_templates.xml',
+        'views/website_ppdb_menu.xml',
+    ],
+    'installable': True,
+    'application': True,
+}
---
+++ website_ppdb/controller/main.py
@@ -0,0 +1,27 @@
+from odoo import http
+from odoo.http import request
+
+class WebsitePPDB(http.Controller):
+
+    @http.route('/ppdb', type='http', auth='public', website=True)
+    def ppdb_form(self, **kwargs):
+        """Render the PPDB form"""
+        return request.render('website_ppdb.ppdb_form_template')
+
+    @http.route('/ppdb/submit', type='http', auth='public', website=True, csrf=False)
+    def submit_ppdb(self, **post):
+        """Handle PPDB form submission"""
+        # Create a new record in 'op.ppdb' model
+        vals = {
+            'nama_lengkap': post.get('nama_lengkap'),
+            'nama_panggilan': post.get('nama_panggilan'),
+            'jenis_kelamin': post.get('jenis_kelamin'),
+            'nisn': post.get('nisn'),
+            'nik': post.get('nik'),
+            'tempat_lahir': post.get('tempat_lahir'),
+            'tgl_lahir': post.get('tgl_lahir'),
+            'akte': post.get('akte'),
+            'agama': post.get('agama'),
+        }
+        request.env['op.ppdb'].sudo().create(vals)
+        return request.render('website_ppdb.ppdb_success_template')
